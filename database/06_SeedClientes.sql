fc676e3b214d5dd847674564abc3b3987618e89f
kevinarrascaeta4-design <kevinarrascaeta4@gmail.com>
2026-07-12 12:03:36 -0300

    Agregar datos iniciales de cliente

---
+++ database/06_SeedClientes.sql
@@ -0,0 +1,19 @@
+USE MetalurgicaWeb;
+GO
+INSERT INTO Cliente (Cuit, Email, Telefono, Direccion, RazonSocial, Activo)
+VALUES
+('30712345678', 'compras@constructorahorizonte.com', '3514501001', 'Av. Colón 1250, Córdoba', 'Constructora Horizonte S.A.', 1),
+('30712345679', 'ventas@aceroscentro.com', '35145011002', 'Av. Japón 4200, Córdoba', 'Aceros del Centro S.R.L', 1),
+('30712345680', 'contacto@ingenieriadelta.com', '3514501003', 'Bv. Los Alemanes 3800, Córdoba', 'Ingenieria Delta S.A.', 1),
+('30712345681', 'compras@transportesdelsur.com', '3514501004', 'Ruta 9 Km 695, Córdoba', 'Transportes del Sur S.A.', 1),
+('30712345682', 'info@metalurgicasanmartin.com', '3514501005', 'Av. Fuerza Aérea 5100, Córdoba', 'Metalurgica San Martín', 1),
+('30712345683', 'ventas@agromaq.com', '3514501006', 'Ruta 19 Km 320, Córdoba', 'AgroMaq S.A.', 1),
+('30712345684', 'compras@atlasconstrucciones.com', '3514501007', 'Av. Circunvalación 2100, Córdoba', 'Construcciones Atlas S.A.', 1),
+('30712345685', 'contacto@grupoindustrialnorte.com', '3514501008', 'Av. Sabattini 1800, Córdoba', 'Grupo Industrial Norte', 1),
+('30712345686', 'info@talleresbelgrano.com', '3514501009', 'Bv. Perón 640, Córdoba', 'Talleres Belgrano', 1),
+('30712345687', 'ventas@hormigonescordoba.com', '3514501010', 'Camino a Capilla de los Remedios 8500', 'Hormigones Córdoba S.A.', 1),
+('30712345688', 'compras@ferroindustrias.com', '3514501011', 'Av. O´Higgins 3400, Córdoba', 'Ferro Industrias S.R.L.', 1),
+('30712345689', 'contacto@estructurasdelcentro.com', '3514501012', 'Av. Vélez Sarsfueld 5200, Córdoba', 'Estructuras del Centro', 1),
+('30712345690', 'ventas@constructoraandes.com', '35145010013', 'Ruta 20 Km 8, Córdoba', 'Constructura Andes S.A.', 1),
+('30712345691', 'info@industriasnova.com', '3514501014', 'Parque Industrial Ferreyra, Córdoba', 'Industrias Nova', 1),
+('30712345692', 'compras@tecnometal.com', '3514501015', 'Av. Juan B. Justo 4100, Córdoba', 'TecnoMetal S.R.L.', 1);
