22afd1148a8fb507c8af2428899f448720617c89
kevinarrascaeta4-design <kevinarrascaeta4@gmail.com>
2026-07-16 11:50:29 -0300

    chore: inicializar frontend con Vite, React y TypeScript

--- backend/MetalurgicaWeb/Metalurgica.API/obj/Debug/net9.0/Metalurgica.API.AssemblyInfo.cs
+++ backend/MetalurgicaWeb/Metalurgica.API/obj/Debug/net9.0/Metalurgica.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Metalurgica.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee16317bb1d483baa3f182025ddea45144573126")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5e55abb38d6fa4e65a0777fafe0976fb8652a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Metalurgica.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Metalurgica.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
